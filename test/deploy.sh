f87c2fab7ab55ced25c24c870932656d657f0dba
Mihai Balint <balint.mihai@gmail.com>
2020-08-21 01:41:33 +0300

    feat: pull in the repository with the hosting spec

--- test/deploy.sh
+++ test/deploy.sh
@@ -1,22 +1,10 @@
 #!/bin/bash
 set -e
 
-# Dump env for debugging purposes
-env
-
 REPO=$(git remote get-url --push origin | sed -e "s/[.]git$/$2/g" | sed -e "s/^.*\///g")
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
-APP="$REPO-$BRANCH"
-TAG=$(git rev-parse --short HEAD)
-FULL_TAG="registry.app.opentsr.com/cityradar/$APP:$TAG"
-
-# Dump tag extracted via git
-echo $FULL_TAG
-
-REPO=transit-ui
 BRANCH="$TRAVIS_BRANCH"
 APP="$REPO-$BRANCH"
-TAG="$TRAVIS_COMMIT"
+TAG=$(git rev-parse --short HEAD)
 FULL_TAG="registry.app.opentsr.com/cityradar/$APP:$TAG"
 
 if [[ -n "$TRAVIS_TAG" || ( "$TRAVIS_PULL_REQUEST" = "false") ]]; then
@@ -24,4 +12,9 @@ if [[ -n "$TRAVIS_TAG" || ( "$TRAVIS_PULL_REQUEST" = "false") ]]; then
   echo "Building and publishing: $APP:$TAG"
   docker login -u "$KUBE_DOCKER_USER" -p "$KUBE_DOCKER_AUTH" registry.app.opentsr.com
   docker build -f Dockerfile -t "$FULL_TAG" . && docker push "$FULL_TAG"
+  rm -rf deployment
+  git clone -b main git@github.com:opentransportro/kube-hosting.git deployment
+  export KUBECONFIG="$(pwd)/deployment/kubeconfig/open-transport-kubeconfig.yaml"
+  kubectl get pods
+  rm -rf deployment
 fi
