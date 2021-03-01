514f2b59dabcc626069a47e24adb5939de471757
ShIulia <iulia.tamazlicariu@gmail.com>
2021-03-01 16:33:53 +0200

    Bug fixes + style updates

--- app/component/DepartureListHeader.js
+++ app/component/DepartureListHeader.js
@@ -3,19 +3,19 @@ import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import ComponentUsageExample from './ComponentUsageExample';
 
-const DepartureListHeader = props => (
+const DepartureListHeader = (props) => (
   <div className="departure-list-header row padding-vertical-small">
-    {!props.staticDeparture && (
-      <span className="time-header">
-        <FormattedMessage id="leaves" defaultMessage="Leaves" />
-      </span>
-    )}
     <span className="route-number-header">
       <FormattedMessage id="route" defaultMessage="Route" />
     </span>
     <span className="route-destination-header">
       <FormattedMessage id="destination" defaultMessage="Destination" />
     </span>
+    {!props.staticDeparture && (
+      <span className="time-header">
+        <FormattedMessage id="leaves" defaultMessage="Leaves" />
+      </span>
+    )}
   </div>
 );
 
