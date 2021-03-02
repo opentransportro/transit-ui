54dc6772a866787a8affc40dbc2b3bded55bd042
ShIulia <32978149+ShIulia@users.noreply.github.com>
2021-03-02 08:33:51 +0200

    Bug fixes + style updates (#73)
    * Bug fixes + style updates
    
    * fix: props DepartureListHeader
    
    * fix: added blank rows
    
    * Delete yarn-error.log
    
    Co-authored-by: Vlad Vesa <hello@vladvesa.ro>

--- app/component/DepartureListHeader.js
+++ app/component/DepartureListHeader.js
@@ -5,17 +5,17 @@ import ComponentUsageExample from './ComponentUsageExample';
 
 const DepartureListHeader = props => (
   <div className="departure-list-header row padding-vertical-small">
-    {!props.staticDeparture && (
-      <span className="time-header">
-        <FormattedMessage id="leaves" defaultMessage="Leaves" />
-      </span>
-    )}
     <span className="route-number-header">
       <FormattedMessage id="route" defaultMessage="Route" />
     </span>
     <span className="route-destination-header">
       <FormattedMessage id="destination" defaultMessage="Destination" />
     </span>
+    {!props.staticDeparture && (
+      <span className="time-header">
+        <FormattedMessage id="leaves" defaultMessage="Leaves" />
+      </span>
+    )}
   </div>
 );
 
